8bd32307a2accc278ebb028b146174d668a86ddf
Nawaz Danish <nawazdanish1996@gmail.com>
2022-12-01 10:52:19 +0530

    updated

---
+++ Node, Express & MongoDB/01 Node/Learning/08 Module Wrapper/index.js
@@ -0,0 +1,15 @@
+/*
+exports: A reference to the module.exports that is shorter to type.
+require: Used to import modules.
+module: A reference to the current module.
+__dirnane:   The directory name of the current module.
+            This is the same as the path.dirname() of the _filename.
+            example:    console.log(__dirname)
+                        console.log(__filename)
+
+*/
+
+console.log("Just Started");
+// console.log( __dirname);
+// console.log(__filename);
+// console.log(module);
