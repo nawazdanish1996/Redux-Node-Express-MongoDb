dcbcfa48f1f59340cc5e91a102fe6797bd0bef88
Nawaz Danish <nawazdanish1996@gmail.com>
2022-12-13 14:48:28 +0530

    updated

--- Node/05/index.js
+++ Node/05/index.js
@@ -2,12 +2,37 @@ const fs = require("fs");
 
 
 // Write File (Create a new file)
-fs.writeFileSync('read.txt', 'Nawaz Danish');
+// fs.writeFileSync('read.txt', 'Nawaz Danish');
+
+// Append file
+// fs.appendFileSync("read.txt", " How are you? ");
 
 // Read File
-fs.readFile('read.txt', 'utf-8', (err, data)=>{
-    console.log(data);
-});
+// fs.readFile('read.txt', 'utf-8', (err, data)=>{
+//     console.log(data);
+// });
+
+// Read File
+const data = fs.readFileSync('read.txt');
+// console.log(data);
+console.log(data.toString());
+// output: <Buffer 4e 61 77 61 7a 20 44 61 6e 69 73 68 75 74 66 2d 38>
+// Buffer is mainly used to store binary data.
+
+// Rename File
+fs.renameSync('read.txt', 'mister.txt');
+
+
+
+
+
+
+
+
+
+
+
+
 
 // try {
     
---
+++ Node/06/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const path = require("path");
+
+// directory create
+// fs.mkdirSync("Nawaz");
+
+// create file bio.txt and data
+// fs.writeFileSync('Nawaz/bio.txt', "I am Nawaz Danish");
+
+// add more data in same file
+// fs.appendFileSync('Nawaz/bio.txt', " from Lodhan Chowk");
+
+// read data without Buffer data
+// fs.readFile('Nawaz/bio.txt', 'utf-8', (err, data)=>{
+//     console.log(data);
+// })
+
+// rename the file name mybio.txt
+// fs.renameSync('Nawaz/bio.txt', 'Nawaz/mybio.txt');
+
+// delete file
+// fs.unlinkSync('Nawaz/mybio.txt');
+
+// delete folder
+// fs.rmdirSync("Nawaz");
