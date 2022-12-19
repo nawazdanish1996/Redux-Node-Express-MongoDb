6c71969896b2e264d0d656e4e92f9aa970208704
Nawaz Danish <nawazdanish1996@gmail.com>
2022-12-19 11:22:43 +0530

    updated

--- Express/30/src/index.js
+++ Express/30/src/index.js
@@ -2,20 +2,39 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 4500 || process.env.port;
+const hbs = require('hbs');
 
 // console.log(path.join(__dirname, '../public'));
 
 // path
 const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // to set the view engine
 app.set('view engine', 'hbs');
+app.set('views', templatePath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(staticPath));
 
 // template engine root
 app.get('/', (req, resp)=>{
     resp.render("index", {
-        channelName: 'YouTube'
+        channelName: 'Nawaz Danish'
     });
+})
+app.get('/about', (req, resp)=>{
+    resp.render('about');
+})
+app.get('/portfolio', (req, resp)=>{
+    resp.render('portfolio');
+})
+app.get('/contact', (req, resp)=>{
+    resp.render('contact');
+})
+app.get('*', (req, resp)=>{
+    resp.render('page404');
 })
 
 // built in middleware
